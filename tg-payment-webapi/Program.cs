89249aba08a72428359e4f0e756815701adb9ed7
DennisNew1 <dennis.neumann@smail.th-koeln.de>
2024-01-24 20:32:26 +0100

    Allow swagger outside of dev mode

--- tg-payment-webapi/Program.cs
+++ tg-payment-webapi/Program.cs
@@ -56,11 +56,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 app.UseCors(PaymentAllowSpecificOrigins);
